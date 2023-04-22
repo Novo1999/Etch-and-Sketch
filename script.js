a627d787d7c49d440a60737733791dfc79d43072
Novo <120156959+Novo1999@users.noreply.github.com>
2023-04-22 21:17:03 +0600

    ok

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 "use strict";
 
 // Creating the grid dynamically
-const container = document.querySelector(".container");
+const etchPad = document.querySelector(".container");
+// Selecting
+const input = document.querySelector(".slider");
+const gridItem = document.querySelectorAll(".grid-item");
+
+function clearPad() {}
 
 function makeRows(rows, cols) {
-  container.style.setProperty("--grid-row", rows);
-  container.style.setProperty("--grid-col", cols);
+  etchPad.style.setProperty("--grid-row", rows);
+  etchPad.style.setProperty("--grid-col", cols);
   for (let i = 0; i < rows * cols; i++) {
     let cell = document.createElement("div");
-    // cell.innerText = i + 1;
-    container.appendChild(cell).className = "grid-item";
+    etchPad.appendChild(cell).className = "grid-item";
   }
 }
 
 makeRows(16, 16);
+
+input.addEventListener("click", (rows, cols) => {
+  makeRows(0, 0);
+  let inputInt = parseInt(input.value);
+  console.log(inputInt);
+
+  for (let i = 1; i <= 100; i++) {
+    if (inputInt > 1 && inputInt <= 100) {
+      rows = inputInt;
+      cols = inputInt;
+    }
+    makeRows(rows, cols);
+    break;
+  }
+});
+// Side Menu
+// Slider for the grids
+// Color Mode - Polychromatic
+// Eraser
+// Sketchpad Clear
