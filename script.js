155f680eb68d40551179d343655e3d7c8b9e67bc
Novo <120156959+Novo1999@users.noreply.github.com>
2023-04-23 00:38:02 +0600

    grid done

--- script.js
+++ script.js
@@ -4,32 +4,46 @@
 const etchPad = document.querySelector(".container");
 // Selecting
 const input = document.querySelector(".slider");
-const gridItem = document.querySelectorAll(".grid-item");
+// Clears the pad
+function clearPad() {
+  while (etchPad.firstChild) {
+    etchPad.removeChild(etchPad.firstChild);
+  }
+}
 
-function clearPad() {}
+// Grid Counter Selector
+
+const gridCounter = document.querySelectorAll(".gridCounter");
+
+gridCount(16);
+
+function gridCount(count) {
+  gridCounter.forEach((e) => {
+    e.textContent = count;
+  });
+}
 
 function makeRows(rows, cols) {
   etchPad.style.setProperty("--grid-row", rows);
   etchPad.style.setProperty("--grid-col", cols);
   for (let i = 0; i < rows * cols; i++) {
     let cell = document.createElement("div");
-    etchPad.appendChild(cell).className = "grid-item";
+    etchPad.appendChild(cell).id = "grid-item";
   }
 }
-
 makeRows(16, 16);
 
 input.addEventListener("click", (rows, cols) => {
-  makeRows(0, 0);
+  clearPad();
   let inputInt = parseInt(input.value);
   console.log(inputInt);
-
   for (let i = 1; i <= 100; i++) {
-    if (inputInt > 1 && inputInt <= 100) {
+    if (inputInt >= 1 && inputInt <= 100) {
       rows = inputInt;
       cols = inputInt;
     }
     makeRows(rows, cols);
+    gridCount(rows);
     break;
   }
 });
